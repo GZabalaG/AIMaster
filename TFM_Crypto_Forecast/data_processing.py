986a9a3e4df5031beb2cb9376204865d1a1377f3
GZabalaG <78758188+GZabalaG@users.noreply.github.com>
2021-12-25 18:41:08 +0100

    new utils files

---
+++ TFM_Crypto_Forecast/data_processing.py
@@ -0,0 +1,2 @@
+#Class with methods for data loading, cleaning and feature extraction from source
+#Future methods to load streaming data and upload live forecast
---
+++ TFM_Crypto_Forecast/utils.py
@@ -0,0 +1,1 @@
+# Utils methods for this project
