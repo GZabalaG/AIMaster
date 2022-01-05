2ab7e04742403ab370b3d2301d96706d671c0cf1
GZabalaG <78758188+GZabalaG@users.noreply.github.com>
2022-01-05 13:49:35 +0100

    n

--- TFM_Crypto_Forecast/crypto_utils.py
+++
@@ -1,9 +0,0 @@
-# Utils methods for this project
-
-class Features:
-
-    def __init__(self): # Constructor
-        pass
-
-    def high_low(row, column):
-        return 1 if row[column] > 0 else 0
